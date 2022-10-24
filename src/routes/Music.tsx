81676550ff328d6a4bde20162f397986ff9bfff3
RiverTwilight <wrj2014@126.com>
2022-10-24 11:13:42 +0800

    other: import assets

---
+++ src/routes/Music.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import Progress from "../components/Progress";
+
+const Pair = () => {
+	return (
+		<>
+			Lock
+			<Progress progress={0.5} />
+		</>
+	);
+};
+
+export default Pair;
